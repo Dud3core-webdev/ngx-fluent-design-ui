d97ebcbf59fa23c4cf6561dcf10c8076673fa4f9
Dud3core-webdev <dudecor3webdev@gmail.com>
2022-01-06 18:16:29 +0000

    Updated docs to include compound button

---
+++ src/assets/docs/code-examples/cta/standard-cta/compound-input-interface.example.ts
@@ -0,0 +1,8 @@
+import { EventEmitter } from '@angular/core';
+
+{
+    disabled: boolean;
+    type: 'standard' | 'primary';
+    clicked: EventEmitter;
+    secondaryText: string;
+}
---
+++ src/assets/docs/code-examples/cta/standard-cta/standard-cta-usage.example.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    template: `
+        <ngx-fluent-design-button [disabled]="false" type="primary">
+            This is an example
+        </ngx-fluent-design-button>
+    `
+})
+export class StandardCtaUsageExample {}
