889b2ce2a4e0d569ac91c7905a3c605ad47ff528
Dud3core-webdev <dudecor3webdev@gmail.com>
2022-01-22 14:59:55 +0000

    Psueodcode for deploy script

--- deploy-to-npm.ps1
+++ deploy-to-npm.ps1
@@ -2,8 +2,30 @@ param( [bool]$isPatch = $true, [bool]$isMinorUpdate = $false )
 
 if(($isPatch -eq $true) -and ($isMinorUpdate -eq $false)) {
     Write-Host "Is a patch only"
+
+    # cd into projects/ngx-fluent-design
+    # npm version command to upgrade patch version
+
+   # cd back to root
+   # git add .
+   # git commit -m "new patch version: {{versionNumber}}"
 }
 
 if(($isPatch -eq $false) -and ($isMinorUpdate -eq $true)) {
     Write-Host "Is a minor update"
+
+    # cd into projects/ngx-fluent-design
+    # npm version command to upgrade minor version
+
+    # cd back to root
+    # git add .
+    # git commit -m "new minor version: {{versionNumber}}"
 }
+
+    # git push -u origin master <-- We might need to pass a branch name in, will see.
+
+    # cd back to root
+    # ng b ngx-fluent-design --prod
+    # cd dist/ngx-fluent-design
+    # npm publish
+
