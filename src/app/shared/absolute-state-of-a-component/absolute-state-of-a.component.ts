4323f479181679c7b91f8211d1518117df3f6b28
Dud3core-webdev <dudecor3webdev@gmail.com>
2022-01-25 22:56:03 +0000

    Nearly everything adheres to the proper typography rules set in the figma design file now, caused some regressions in checkboxes and radio field labels, but it should be easy to fix.

--- src/app/shared/absolute-state-of-a-component/absolute-state-of-a.component.ts
+++ src/app/shared/absolute-state-of-a-component/absolute-state-of-a.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { NgxFluentDesignIconInfoWarn } from '../../../../projects/ngx-fluent-design/src/lib/icons/shared/constants/ngx-fluent-design-icons-list';
 
@@ -7,7 +7,9 @@ import { NgxFluentDesignIconInfoWarn } from '../../../../projects/ngx-fluent-des
     templateUrl: './absolute-state-of-a.component.html',
     styleUrls: ['./absolute-state-of-a.component.scss']
 })
-export class AbsoluteStateOfAComponent {
+export class AbsoluteStateOfAComponent implements AfterViewInit {
+    public progressIndicatorCurrentValue: number = 0;
+
     testForm = new FormGroup({
         checkboxEnabled: new FormControl(''),
         checkboxDisabled: new FormControl(true),
@@ -17,6 +19,19 @@ export class AbsoluteStateOfAComponent {
     tesIconList = {
         warningIcon: NgxFluentDesignIconInfoWarn
     };
+
+    ngAfterViewInit(): void {
+        this.exampleProgressLoad();
+    }
+
+    public exampleProgressLoad(percentage = 0): void {
+        if (percentage >= 100) {
+            return;
+        }
+        this.progressIndicatorCurrentValue = percentage;
+        requestAnimationFrame(() => this.exampleProgressLoad(this.progressIndicatorCurrentValue + 0.05));
+    }
+
 
     public exampleAlert(componentType: string): void {
         window.alert(`This is a ${componentType} click event`);
