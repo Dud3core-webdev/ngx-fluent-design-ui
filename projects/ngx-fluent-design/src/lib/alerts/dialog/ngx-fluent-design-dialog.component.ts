e5a2e23a935a9a20f8ed32325221275a0eb49b53
Dud3core-webdev <dudecor3webdev@gmail.com>
2022-01-26 22:14:16 +0000

    Added more mixins for font colors, loving em.
    Enforced typography rules fully
    
    Refactored buttons to not be so rigid and letting all its content overflow
    
    Fixed dialog component, added an interface and unit tested it.

--- projects/ngx-fluent-design/src/lib/alerts/dialog/ngx-fluent-design-dialog.component.ts
+++ projects/ngx-fluent-design/src/lib/alerts/dialog/ngx-fluent-design-dialog.component.ts
@@ -1,19 +1,22 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { INgxFluentDesignIcon } from '../../icons/shared/types/ngx-fluent-design-icon.interface';
 import { NgxFluentDesignIconClearClose } from '../../icons/shared/constants/ngx-fluent-design-icons-list';
+import { INgxFluentDesignDialog } from '../shared/types/ngx-fluent-design-dialog.interface';
 
 @Component({
     selector: 'ngx-fluent-design-dialog',
     templateUrl: './ngx-fluent-design-dialog.component.html',
     styleUrls: ['./ngx-fluent-design-dialog.component.scss']
 })
-export class NgxFluentDesignDialogComponent {
+export class NgxFluentDesignDialogComponent implements INgxFluentDesignDialog {
     @Input() public displaySecondaryAction: boolean = true;
     @Input() public primaryActionName: string = 'Primary Action';
     @Input() public secondaryActionName: string = 'Secondary Action';
+    @Input() public header: string = '';
 
-    @Output() public primaryActionClicked: EventEmitter<void> = new EventEmitter<void>();
-    @Output() public secondaryActionClicked: EventEmitter<void> = new EventEmitter<void>();
+    @Output() public readonly primaryActionClicked: EventEmitter<void> = new EventEmitter<void>();
+    @Output() public readonly secondaryActionClicked: EventEmitter<void> = new EventEmitter<void>();
+    @Output() public readonly closeIconClicked: EventEmitter<void> = new EventEmitter<void>();
 
     public readonly closeIcon: INgxFluentDesignIcon = NgxFluentDesignIconClearClose;
 }
