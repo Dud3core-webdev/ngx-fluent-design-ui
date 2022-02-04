d110bf4b21d877100fa6cd83c273e286aa0f726f
Dud3core-webdev <dudecor3webdev@gmail.com>
2022-02-04 17:03:39 +0000

    Upgraded package version to 0.3.4

--- projects/ngx-fluent-design/src/lib/common/components/ngx-fluent-design-common-components.module.ts
+++ projects/ngx-fluent-design/src/lib/common/components/ngx-fluent-design-common-components.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgxFluentDesignSheetComponent } from './sheet/ngx-fluent-design-sheet.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 const DECLARATIONS: Array<any> = [
     NgxFluentDesignSheetComponent
 ];
 
 @NgModule({
-    imports: [
-        CommonModule,
-        BrowserAnimationsModule
-    ],
+    imports: [CommonModule],
     declarations: [...DECLARATIONS],
     exports: [...DECLARATIONS]
 })
