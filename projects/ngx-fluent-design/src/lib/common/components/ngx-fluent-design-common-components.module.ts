18e498d47edde77655ee4a97823f5b831483d296
Dud3core-webdev <dudecor3webdev@gmail.com>
2022-02-04 13:50:05 +0000

    Added a nice little fade in animation for the sheet component

--- projects/ngx-fluent-design/src/lib/common/components/ngx-fluent-design-common-components.module.ts
+++ projects/ngx-fluent-design/src/lib/common/components/ngx-fluent-design-common-components.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgxFluentDesignSheetComponent } from './sheet/ngx-fluent-design-sheet.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 const DECLARATIONS: Array<any> = [
     NgxFluentDesignSheetComponent
 ];
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [
+        CommonModule,
+        BrowserAnimationsModule
+    ],
     declarations: [...DECLARATIONS],
     exports: [...DECLARATIONS]
 })
--- projects/ngx-fluent-design/src/lib/common/components/sheet/ngx-fluent-design-sheet.component.ts
+++ projects/ngx-fluent-design/src/lib/common/components/sheet/ngx-fluent-design-sheet.component.ts
@@ -1,8 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, Inject, Input } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
     selector: 'ngx-fluent-design-sheet',
     templateUrl: './ngx-fluent-design-sheet.component.html',
-    styleUrls: ['./ngx-fluent-design-sheet.component.scss']
+    styleUrls: ['./ngx-fluent-design-sheet.component.scss'],
+    animations: [
+        trigger('fadeIn', [
+            state('show', style({
+                opacity: 1,
+                visibility: 'visible'
+            })),
+            state('hide', style({
+                opacity: 0,
+                visibility: 'hidden'
+            })),
+            transition('show => hide', [
+                animate('150ms')
+            ]),
+            transition('hide => show', [
+                animate('150ms')
+            ]),
+        ])
+    ]
 })
-export class NgxFluentDesignSheetComponent {}
+export class NgxFluentDesignSheetComponent {
+
+    private readonly _document: Document;
+    private readonly _noScrollClassName: string = 'no-scroll';
+    private _hidden: boolean = false;
+
+    constructor(@Inject(DOCUMENT) document: Document) {
+        this._document = document;
+    }
+
+    @Input()
+    public set hidden(isHidden: boolean) {
+        this._hidden = isHidden;
+        this.toggleNoScrollBodyElement(isHidden);
+    }
+
+    public get hidden(): boolean {
+        return this._hidden;
+    }
+
+    private toggleNoScrollBodyElement(isHidden: boolean): void {
+        if (isHidden) {
+            this._document.body.classList.remove(this._noScrollClassName);
+        } else {
+            this._document.body.classList.add(this._noScrollClassName);
+        }
+    }
+}
