f37bcdfdb096e433fbae8f1c863031a77c8c650b
Michal Hajdo <smephey@gmail.com>
2022-02-11 14:33:40 +0000

    Testing if this fixes cypress on github

--- src/app/pages/test-form/test-form-page.module.ts
+++ src/app/pages/test-form/test-form-page.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
-import { NgxFluentDesignCommonComponentsModule } from 'projects/ngx-fluent-design/src/lib/common/components/ngx-fluent-design-common-components.module';
-import { NgxFluentDesignNotificationModule } from 'projects/ngx-fluent-design/src/lib/notifications/ngx-fluent-design-notification.module';
 import { SharedModule } from '../../shared/shared.module';
 import { TestFormPageComponent } from './test-form-page.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { NgxFluentDesignInputModule } from 'projects/ngx-fluent-design/src/lib/input/ngx-fluent-design-input.module';
-import { NgxFluentDesignCtaModule } from 'projects/ngx-fluent-design/src/lib/cta/ngx-fluent-design-cta.module';
 import { CommonModule } from '@angular/common';
-import { NgxFluentDesignAlertModule } from 'projects/ngx-fluent-design/src/lib/alerts/ngx-fluent-design-alert.module';
-import { NgxFluentDesignProgressModule } from 'ngx-fluent-design';
+import { NgxFluentDesignCommonComponentsModule } from '../../../../projects/ngx-fluent-design/src/lib/common/components/ngx-fluent-design-common-components.module';
+import { NgxFluentDesignNotificationModule } from '../../../../projects/ngx-fluent-design/src/lib/notifications/ngx-fluent-design-notification.module';
+import { NgxFluentDesignInputModule } from '../../../../projects/ngx-fluent-design/src/lib/input/ngx-fluent-design-input.module';
+import { NgxFluentDesignCtaModule } from '../../../../projects/ngx-fluent-design/src/lib/cta/ngx-fluent-design-cta.module';
+import { NgxFluentDesignProgressModule } from '../../../../projects/ngx-fluent-design/src/lib/progress/ngx-fluent-design-progress.module';
+import { NgxFluentDesignAlertModule } from '../../../../projects/ngx-fluent-design/src/lib/alerts/ngx-fluent-design-alert.module';
 
 @NgModule({
     imports: [
