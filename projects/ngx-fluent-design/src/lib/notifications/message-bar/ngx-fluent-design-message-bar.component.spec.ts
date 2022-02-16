004fc66fd03c5495b22fc49ea1707b0809adec14
Dud3core-webdev <dudecor3webdev@gmail.com>
2022-02-16 16:23:05 +0000

    Updated tests to document logic better.

--- projects/ngx-fluent-design/src/lib/notifications/message-bar/ngx-fluent-design-message-bar.component.spec.ts
+++ projects/ngx-fluent-design/src/lib/notifications/message-bar/ngx-fluent-design-message-bar.component.spec.ts
@@ -50,7 +50,8 @@ describe('NgxFluentDesignMessageBarComponent', () => {
                 expect(dismissCTA).toBeNull();
             });
         });
-        describe('And "action" and "dismiss" CTAs are enabled', () => {
+
+        describe('When "action" and "dismiss" CTAs are enabled', () => {
             beforeEach(() => {
                 component.actionName = 'Hello';
                 component.handler = new NgxFluentDesignMessageBarHandler(true);
@@ -62,27 +63,91 @@ describe('NgxFluentDesignMessageBarComponent', () => {
                 const actionButton = fixture.debugElement.query(bySpecAttribute('fluent-design-message-bar-action'));
                 expect(actionButton).not.toBeNull();
             });
+
             it('Then the message bar should display the "dismiss" CTA', () => {
                 const dismissCTA = fixture.debugElement.query(bySpecAttribute('fluent-design-message-bar-dismiss-CTA'));
                 expect(dismissCTA).not.toBeNull();
             });
 
-            describe('And the user clicks on the "action" button', () => {
+            describe('When the user clicks on the "action" button', () => {
                 beforeEach(() => {
                     const actionButton = fixture.debugElement.query(bySpecAttribute('fluent-design-message-bar-action')).componentInstance;
                     actionButton.clicked.emit();
                 });
+
                 it('Then the actionClickedEvent should emit', () => {
                     expect(actionClickedEventSpy).toHaveBeenCalled();
                 });
             });
-            describe('And the user clicks on the "dismiss" CTA', () => {
+
+            describe('When the user clicks on the "dismiss" CTA', () => {
                 beforeEach(() => {
                     const dismissCTA = fixture.debugElement.query(bySpecAttribute('fluent-design-message-bar-dismiss-CTA')).nativeElement;
                     dismissCTA.click();
                 });
+
                 it('Then the actionClickedEvent should emit', () => {
                     expect(dismissClickedEventSpy).toHaveBeenCalled();
+                });
+            });
+        });
+
+        describe('When the handler is not present', () => {
+            beforeEach(() => {
+                component.actionName = 'Hello';
+                fixture.detectChanges();
+            });
+
+            it('Then the close icon cannot be displayed', () => {
+                expect(component.canDisplayCloseLogo).toBe(false);
+            });
+
+            it('Then the close icon is not displayed on the DOM', () => {
+                const dismissCTA = fixture.debugElement.query(bySpecAttribute('fluent-design-message-bar-dismiss-CTA'));
+                expect(dismissCTA).toBeNull();
+            });
+
+            it('Then the action bar can still be displayed', () => {
+                const actionButton = fixture.debugElement.query(bySpecAttribute('fluent-design-message-bar-action')).componentInstance;
+
+                expect(actionButton).toBeDefined();
+            });
+
+            it('Then the actionClickedEvent should emit', () => {
+                const actionButton = fixture.debugElement.query(bySpecAttribute('fluent-design-message-bar-action')).componentInstance;
+                actionButton.clicked.emit();
+
+                expect(actionClickedEventSpy).toHaveBeenCalled();
+            });
+        });
+
+        describe('When the handler is defaulted to false', () => {
+
+            let container;
+
+            beforeEach(() => {
+                component.actionName = 'Hello';
+                component.handler = new NgxFluentDesignMessageBarHandler(false);
+                component.handler.close();
+                fixture.detectChanges();
+
+                container = fixture.debugElement.query(bySpecAttribute('ngx-fluent-design-message-bar'));
+            });
+
+            it('Then the entire component isn\'t present', () => {
+                expect(container.nativeElement.style.visibility).toBe('hidden');
+            });
+
+            describe('And the handler state is changed', () => {
+                beforeEach(() => {
+                    component.handler.open();
+                    fixture.detectChanges();
+
+                    container = fixture.debugElement.query(bySpecAttribute('ngx-fluent-design-message-bar'));
+                });
+
+                it('Then the entire component is visible', () => {
+                    expect(container.nativeElement.style.visibility).toBe('visible');
                 });
             });
         });
