85259706fe285fae42640f047cf334303f679a8b
Smephey <smephey@gmail.com>
2022-02-25 14:44:30 +0000

    Don't actually need to listen to status changes at this current level of functionality.

--- projects/ngx-fluent-design/src/lib/input/shared/directives/ngx-fluent-design-input-class-validator.directive.ts
+++ projects/ngx-fluent-design/src/lib/input/shared/directives/ngx-fluent-design-input-class-validator.directive.ts
@@ -1,20 +1,14 @@
-import { Directive, HostBinding, OnInit } from '@angular/core';
+import { Directive, HostBinding } from '@angular/core';
 import { NgControl } from '@angular/forms';
 
 @Directive({
     selector: '[ngxFluentDesignInput]'
 })
-export class NgxFluentDesignInputClassValidatorDirective implements OnInit {
-
+export class NgxFluentDesignInputClassValidatorDirective {
     private readonly _ngControl: NgControl;
 
     constructor(ngControl: NgControl) {
         this._ngControl = ngControl;
-    }
-
-    public ngOnInit(): void {
-        this._ngControl.statusChanges
-            .subscribe((x) => console.log(x));
     }
 
     @HostBinding('class.has-error')
