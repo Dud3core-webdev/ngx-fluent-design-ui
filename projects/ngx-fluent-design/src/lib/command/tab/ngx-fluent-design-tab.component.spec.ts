e0bc924583fe829e3ebc3ecfe3816ed91d4944a4
Dud3core-webdev <dudecor3webdev@gmail.com>
2022-03-16 14:51:57 +0000

    Finished unit test

--- projects/ngx-fluent-design/src/lib/command/tab/ngx-fluent-design-tab.component.spec.ts
+++ projects/ngx-fluent-design/src/lib/command/tab/ngx-fluent-design-tab.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NgxFluentDesignTabComponent } from './ngx-fluent-design-tab.component';
 import { CommonModule } from '@angular/common';
 import { bySpecAttribute } from '../../../testing/test-helper';
+import { DebugElement } from '@angular/core';
+import SpyInstance = jest.SpyInstance;
 
 describe('NgxFluentDesignTabComponent', () => {
     let fixture: ComponentFixture<NgxFluentDesignTabComponent>;
@@ -21,22 +23,70 @@ describe('NgxFluentDesignTabComponent', () => {
 
     describe('Given a clean state for the tab component', () => {
 
+        let mainElement: DebugElement;
+        let commandNameElement: DebugElement;
+
         describe('When isChecked is passed as false', () => {
             beforeEach(() => {
                 component.commandName = 'General Kenobi';
                 component.isChecked = false;
 
                 fixture.detectChanges();
+
+                mainElement = fixture.debugElement.query(bySpecAttribute('fluent-design-tab'));
+                commandNameElement = fixture.debugElement.query(bySpecAttribute('command-name'));
             });
 
             it('Then the active class does not appear', () => {
-                const parentElement = fixture.debugElement.query(bySpecAttribute('fluent-design-tab'));
-                expect(parentElement.nativeElement.classList.contains('active')).toBe(false);
+                expect(mainElement.nativeElement.classList.contains('active')).toBe(false);
             });
 
             it('Then the command name is present', () => {
-                const commandNameElement = fixture.debugElement.query(bySpecAttribute('command-name'));
                 expect(commandNameElement.nativeElement.innerHTML).toBe('General Kenobi');
+            });
+
+            describe('When clicking the element', () => {
+
+                let clickSpy: SpyInstance;
+
+                beforeEach(() => {
+                     clickSpy = jest.spyOn(component.onChecked, 'emit').mockReturnThis();
+                     mainElement.nativeElement.click();
+                });
+
+                it('Then an event is emitted', () => {
+                    expect(component.onChecked.emit).toHaveBeenCalledWith();
+                });
+            });
+        });
+
+        describe('When isChecked is passed as true', () => {
+            beforeEach(() => {
+                component.commandName = 'General Kenobi';
+                component.isChecked = true;
+
+                fixture.detectChanges();
+
+                mainElement = fixture.debugElement.query(bySpecAttribute('fluent-design-tab'));
+                commandNameElement = fixture.debugElement.query(bySpecAttribute('command-name'));
+            });
+
+            it('Then the active class does appear', () => {
+                expect(mainElement.nativeElement.classList.contains('active')).toBe(true);
+            });
+
+            describe('When clicking the element', () => {
+
+                let clickSpy: SpyInstance;
+
+                beforeEach(() => {
+                    clickSpy = jest.spyOn(component.onChecked, 'emit').mockReturnThis();
+                    mainElement.nativeElement.click();
+                });
+
+                it('Then an event is emitted', () => {
+                    expect(component.onChecked.emit).toHaveBeenCalledWith();
+                });
             });
         });
     });
